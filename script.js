18f10e48a5bc6a134d8fa21247290764c4fdb02f
divyanshugupta0 <150168350+divyanshugupta0@users.noreply.github.com>
2024-08-13 00:20:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -73,6 +73,29 @@ $(document).ready(function(){
             }
         }
     });
+    //  =======================================================mobile prohibit==========================================
+
+// Detect if the user is accessing from a mobile device
+function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+// Redirect if the user is accessing from a mobile device
+function redirectIfMobile() {
+    if (isMobile()) {
+        // Redirect to a different page
+        window.location.href = "unsupport.html";
+    }
+}
+
+// Call the function to check and redirect on page load
+window.onload = redirectIfMobile;
+
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    window.location.href = "unsupport.html";
+}
+});
 
 
 
@@ -174,5 +197,4 @@ $(document).ready(function(){
   sendChatBtn.addEventListener("click", handleChat);
   closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
   chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
-
 
